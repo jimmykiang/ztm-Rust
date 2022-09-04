0c2ea743bac3174e244957a98b64b14612c083c9
jimmykiang <castlevanity@gmail.com>
2022-09-04 21:54:20 +0200

    a32

--- activities/src/bin/a32.rs
+++ activities/src/bin/a32.rs
@@ -12,4 +12,32 @@
 
 const MOCK_DATA: &'static str = include_str!("mock-data.csv");
 
-fn main() {}
+struct Names<'a> {
+    names_vector: Vec<&'a str>,
+}
+
+struct Titles<'a> {
+    titles_vector: Vec<&'a str>,
+}
+
+fn zip_in<'a>(data: &Vec<&'a str>, names: &mut Names<'a>, titles: &mut Titles<'a>) {
+    names.names_vector = data.iter().filter_map(|x| x.split(',').nth(1)).collect();
+
+    titles.titles_vector = data.iter().filter_map(|x| x.split(',').nth(4)).collect();
+
+    for (name, title) in names.names_vector.iter().zip(titles.titles_vector.iter()) {
+        println!("Name: {:?}, Title: {:?}", name, title.trim());
+    }
+}
+
+fn main() {
+    let data: Vec<&str> = MOCK_DATA.split('\n').skip(1).collect();
+    let mut names = Names {
+        names_vector: Vec::new(),
+    };
+    let mut titles = Titles {
+        titles_vector: Vec::new(),
+    };
+
+    zip_in(&data, &mut names, &mut titles);
+}
