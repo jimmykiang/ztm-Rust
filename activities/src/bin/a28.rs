1536d2670808dcb3a2bfdace38b21af598bfdf0f
jimmykiang <castlevanity@gmail.com>
2022-09-06 22:46:30 +0200

    NewTypePattern a28

--- activities/src/bin/a28.rs
+++ activities/src/bin/a28.rs
@@ -10,6 +10,7 @@
 // * Create a function for each type of clothes (shoes, shirt, pants)
 //   that accepts the new type specific to that type of clothing
 
+#[derive(Debug)]
 enum Color {
     Black,
     Blue,
@@ -23,4 +24,48 @@ enum Color {
     Yellow,
 }
 
-fn main() {}
+#[derive(Debug)]
+struct ShoesColor(Color);
+impl ShoesColor {
+    fn new(color: Color) -> Self {
+        Self(color)
+    }
+}
+
+#[derive(Debug)]
+struct ShirtColor(Color);
+impl ShirtColor {
+    fn new(color: Color) -> Self {
+        Self(color)
+    }
+}
+
+#[derive(Debug)]
+struct PantsColor(Color);
+impl PantsColor {
+    fn new(color: Color) -> Self {
+        Self(color)
+    }
+}
+
+fn print_shoes_color(x: ShoesColor) {
+    println!("Shoes color: {:?}", x);
+}
+
+fn print_shirt_color(x: ShirtColor) {
+    println!("Shirt color: {:?}", x);
+}
+
+fn print_pants_color(x: PantsColor) {
+    println!("Pants color: {:?}", x);
+}
+
+fn main() {
+    let shoes_color = ShoesColor::new(Color::Custom("Rosita".to_owned()));
+    let pants_color = PantsColor::new(Color::Brown);
+
+    print_shoes_color(shoes_color);
+    print_pants_color(pants_color);
+
+
+}
