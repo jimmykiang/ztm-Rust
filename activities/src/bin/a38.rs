67a0a4028b1071df4089d74f551499e4d3750953
jimmykiang <castlevanity@gmail.com>
2022-09-13 15:26:58 +0200

    a38.

--- activities/src/bin/a38.rs
+++ activities/src/bin/a38.rs
@@ -16,14 +16,25 @@ fn msg_hello() -> &'static str {
 
 fn msg_thread() -> &'static str {
     use std::time::Duration;
-    std::thread::sleep(Duration::from_millis(1000));
+    std::thread::sleep(Duration::from_millis(800));
     "threads"
 }
 
 fn msg_excited() -> &'static str {
     use std::time::Duration;
-    std::thread::sleep(Duration::from_millis(1000));
+    std::thread::sleep(Duration::from_millis(500));
     "!"
 }
 
-fn main() {}
+fn main() {
+    use std::thread;
+    let msg_1 = thread::spawn(move || msg_hello());
+    let msg_2 = thread::spawn(move || msg_thread());
+    let msg_3 = thread::spawn(move || msg_excited());
+
+    let msg_1 = msg_1.join().expect("Error join msg_1");
+    let msg_2 = msg_2.join().expect("Error join msg_2");
+    let msg_3 = msg_3.join().expect("Error join msg_3");
+
+    println!("{:?}{:?}{:?}", msg_1, msg_2, msg_3);
+}
