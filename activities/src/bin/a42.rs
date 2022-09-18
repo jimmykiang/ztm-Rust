f46cdcd0b0d912c6d412bb5ab5c530d35fc71c46
jimmykiang <castlevanity@gmail.com>
2022-09-18 21:33:30 +0200

    a42.rs

--- activities/src/bin/a42.rs
+++ activities/src/bin/a42.rs
@@ -18,4 +18,41 @@
 // * Use the .next() method to advance the iterator to confirm it works correctly
 // * Only the Iterator trait needs to be implemented for this activity
 
-fn main() {}
+struct Score {
+    amount: usize,
+    progression: usize,
+    bonus: usize,
+}
+
+impl Score {
+    fn new() -> Self {
+        Self {
+            amount: 0,
+            progression: 1,
+            bonus: 0,
+        }
+    }
+
+    fn set_bonus(&mut self, x: usize) {
+        self.bonus = x;
+    }
+}
+
+impl Iterator for Score {
+    type Item = usize;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.amount += self.progression + self.bonus;
+        return Some(self.amount);
+    }
+}
+
+fn main() {
+    let mut score = Score::new();
+    println!("{:?}", score.next());
+    println!("{:?}", score.next());
+    println!("{:?}", score.next());
+    score.set_bonus(3);
+    println!("{:?}", score.next());
+    println!("{:?}", score.next());
+    println!("{:?}", score.next());
+}
